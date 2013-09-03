3b99efbd51f8494520fbc774901513e80aaba757
frebro <frebro@gmail.com>
2013-09-03 15:51:32 +0200

    disabling wp updates from admin (use composer)

--- app/themes/open-dai/lib/custom.php
+++ app/themes/open-dai/lib/custom.php
@@ -8,3 +8,17 @@ function remove_editor_menu() {
   remove_action('admin_menu', '_add_themes_utility_last', 101);
 }
 add_action('_admin_menu', 'remove_editor_menu', 1);
+
+//Disable Theme Updates
+remove_action( 'load-update-core.php', 'wp_update_themes' );
+add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return null;" ) );
+wp_clear_scheduled_hook( 'wp_update_themes' );
+
+//Disable Plugin Updates
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+wp_clear_scheduled_hook( 'wp_update_plugins' );
+
+//Disable Core Updates
+add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
+wp_clear_scheduled_hook( 'wp_version_check' );
