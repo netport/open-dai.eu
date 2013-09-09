3cc291c7ac4e8c06349c098c77ecfdb4a51d1836
frebro <frebro@gmail.com>
2013-09-09 17:26:33 +0200

    header transition flicker fix

--- app/themes/open-dai/lib/scripts.php
+++ app/themes/open-dai/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd0ffe65fc0be786a5a1738e74a71447e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5522a625fba3348f51f0809437515b47');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
