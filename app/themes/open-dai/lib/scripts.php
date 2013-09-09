3b57b3eb144e2e48d24e6a12bfe2586782860a31
frebro <frebro@gmail.com>
2013-09-09 13:13:13 +0200

    "skinny" animation hook on logo

--- app/themes/open-dai/lib/scripts.php
+++ app/themes/open-dai/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c8b645ba0415bf6c42e606b11a58d08');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0901158e79a06e8e5ebde5cfd1fcf460');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
