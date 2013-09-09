1a7acf035901605c11f8e6f2db40abd21cd0df36
frebro <frebro@gmail.com>
2013-09-09 11:29:32 +0200

    added favicons

--- app/themes/open-dai/lib/custom.php
+++ app/themes/open-dai/lib/custom.php
@@ -22,3 +22,20 @@ wp_clear_scheduled_hook( 'wp_update_plugins' );
 //Disable Core Updates
 add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
 wp_clear_scheduled_hook( 'wp_version_check' );
+
+// Asset URL rewrites
+function add_asset_rewrites($content) {
+  global $wp_rewrite;
+  $new_non_wp_rules = array(
+    'assets/img/(.*)'      => THEME_PATH . '/assets/img/$1',
+    'assets/fonts/(.*)'      => THEME_PATH . '/assets/fonts/$1',
+  );
+  $wp_rewrite->non_wp_rules = array_merge($wp_rewrite->non_wp_rules, $new_non_wp_rules);
+  return $content;
+}
+
+if (!is_multisite() && !is_child_theme()) {
+  if (current_theme_supports('rewrites')) {
+    add_action('generate_rewrite_rules', 'add_asset_rewrites');
+  }
+}
