fbc30341dcd3a0823b58c6548aa18a12d604bbb8
frebro <frebro@gmail.com>
2013-09-09 22:56:34 +0200

    jumbotron strings

--- app/themes/open-dai/templates/jumbotron.php
+++ app/themes/open-dai/templates/jumbotron.php
@@ -5,7 +5,8 @@
         <img src="<?php echo THEME_PATH; ?>//assets/img/OPEN-DAI.png" alt="Open-DAI: Open Data Architectures and Infrastructures">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-5 col-md-5">
-        <p class="lead">Working toward a European cloud of virtualized public administration data for citizens and companies.</p>
+        <p class="lead">Working toward a European cloud of public administration data and services.</p>
+        <p><abbr title="Software as a Service">SaaS</abbr>&ensp;+&ensp;<abbr title="Platform as a Service">PaaS</abbr>&ensp;+&ensp;<abbr title="Infrastructure as a Service">IaaS</abbr>&emsp;=&emsp;Open-DAI</p>
       </div>
     </div>
   </div>
