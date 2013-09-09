c4652b18838a6d7e5ef905fd36b611d5b8d5f2fc
frebro <frebro@gmail.com>
2013-09-09 13:16:30 +0200

    lib/custom cleanup

--- app/themes/open-dai/lib/custom.php
+++ app/themes/open-dai/lib/custom.php
@@ -3,27 +3,47 @@
  * Custom functions
  */
 
+
+/**
+ * Admin UI
+ */
+
 // Remove the theme editor from admin
 function remove_editor_menu() {
   remove_action('admin_menu', '_add_themes_utility_last', 101);
 }
 add_action('_admin_menu', 'remove_editor_menu', 1);
 
-//Disable Theme Updates
+/**
+ * Disable updates from admin
+ */
+
+// Theme
 remove_action( 'load-update-core.php', 'wp_update_themes' );
 add_filter( 'pre_site_transient_update_themes', create_function( '$a', "return null;" ) );
 wp_clear_scheduled_hook( 'wp_update_themes' );
 
-//Disable Plugin Updates
+// Plugin
 remove_action( 'load-update-core.php', 'wp_update_plugins' );
 add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
 wp_clear_scheduled_hook( 'wp_update_plugins' );
 
-//Disable Core Updates
+// Core
 add_filter( 'pre_site_transient_update_core', create_function( '$a', "return null;" ) );
 wp_clear_scheduled_hook( 'wp_version_check' );
 
-// Asset URL rewrites
+
+/**
+ * URL rewrites
+ */
+
+if (!is_multisite() && !is_child_theme()) {
+  if (current_theme_supports('rewrites')) {
+    add_action('generate_rewrite_rules', 'add_asset_rewrites');
+  }
+}
+
+// Assets
 function add_asset_rewrites($content) {
   global $wp_rewrite;
   $new_non_wp_rules = array(
@@ -34,8 +54,3 @@ function add_asset_rewrites($content) {
   return $content;
 }
 
-if (!is_multisite() && !is_child_theme()) {
-  if (current_theme_supports('rewrites')) {
-    add_action('generate_rewrite_rules', 'add_asset_rewrites');
-  }
-}
