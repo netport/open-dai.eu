f1a17e89bf8e8bebc5a39b7cf60168abe61286c5
frebro <frebro@gmail.com>
2013-09-10 07:09:40 +0200

    custom template for home + project progress bar

---
+++ app/themes/open-dai/templates/progress.php
@@ -0,0 +1,58 @@
+<div class="project-progress panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Project progress</h3>
+  </div>
+  <div class="panel-body">
+
+    <span class="pull-left" style="margin-right:1em;">Feb 2012</span>
+    <span class="pull-right" style="margin-left:1em;">Dec 2014</span>
+    <div class="progress">
+      <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+        <span class="sr-only">60% Complete</span>
+      </div>
+    </div>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th colspan="2">Work packages</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><span class="glyphicon glyphicon-ok text-success"></span></td>
+          <td><a href="#" class="text-success">System and Architecture specification</a></td>
+        </tr>
+        <tr>
+          <td><span class="glyphicon glyphicon-ok text-success"></span></td>
+          <td><a href="#" class="text-success">Data assessment and specification</a></td>
+        </tr>
+        <tr>
+          <td><span class="glyphicon glyphicon-arrow-right text-primary"></span></td>
+          <td><a href="#" class="text-primary">Management</a></td>
+        </tr>
+        <tr>
+          <td><span class="glyphicon glyphicon-arrow-right text-primary"></span></td>
+          <td><a href="#" class="text-primary">System/Platform implementation</a></td>
+        </tr>
+        <tr>
+          <td><span class="glyphicon glyphicon-arrow-right text-primary"></span></td>
+          <td><a href="#" class="text-primary">Pilot Development</a></td>
+        </tr>
+        <tr>
+          <td><span class="glyphicon glyphicon-arrow-right text-primary"></span></td>
+          <td><a href="#" class="text-primary">Dissemination</a></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><a href="#" class="text-muted">Services Development, implementation and testing</a></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td><a href="#" class="text-muted">Impact evaluation and Exploitation</a></td>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+</div>
