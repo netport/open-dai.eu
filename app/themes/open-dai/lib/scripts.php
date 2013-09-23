6025595f58af023c43e2a4ea6dacd98f6665754f
frebro <frebro@gmail.com>
2013-09-23 13:41:49 +0200

    lighter headings

--- app/themes/open-dai/lib/scripts.php
+++ app/themes/open-dai/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a058e6c1e135460db0d44ee6a7e2e1ff');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3f1635a3b11dc4e16fe8bff88456ee8a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
