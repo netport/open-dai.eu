d14a9a8507312555f97c2afe8f6c0aa1089a759c
frebro <frebro@gmail.com>
2013-09-30 07:29:08 +0200

    border radius defaults

--- app/themes/open-dai/lib/scripts.php
+++ app/themes/open-dai/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6326bdd263d84acd772db647ae50778');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '92626a3df2b0967890bebf4d2f42e944');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
