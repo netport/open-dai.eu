b166ec021ee1ade9095ac19ddfbf8bf2dc32e463
frebro <frebro@gmail.com>
2013-09-30 12:09:15 +0200

    jumbotron img asset is root relative

--- app/themes/open-dai/templates/jumbotron.php
+++ app/themes/open-dai/templates/jumbotron.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-sm-12 col-md-6 col-md-6">
-        <img src="<?php echo THEME_PATH; ?>//assets/img/OPEN-DAI.png" alt="Open-DAI: Open Data Architectures and Infrastructures">
+        <img src="/<?php echo THEME_PATH; ?>/assets/img/OPEN-DAI.png" alt="Open-DAI: Open Data Architectures and Infrastructures">
       </div>
       <div class="col-xs-12 col-sm-12 col-md-5 col-md-5">
         <p class="lead">Working toward a European cloud of public administration data and services.</p>
