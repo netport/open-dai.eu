b33286e6d363d3c7aa54538e539402a40c29aa13
frebro <frebro@gmail.com>
2013-09-30 09:17:06 +0200

    home page grid adjustment

--- app/themes/open-dai/page-home.php
+++ app/themes/open-dai/page-home.php
@@ -1,7 +1,9 @@
-<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-  <?php get_template_part('templates/content', 'page'); ?>
-</div>
+<div class="row">
+  <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+    <?php get_template_part('templates/content', 'page'); ?>
+  </div>
 
-<div class="col-xs-12 col-sm-12 col-md-5 col-md-offset-1 col-lg-5 col-lg-offset-1">
-  <?php get_template_part('templates/progress'); ?>
+  <div class="col-xs-12 col-sm-12 col-md-5 col-md-offset-1 col-lg-5 col-lg-offset-1">
+    <?php get_template_part('templates/progress'); ?>
+  </div>
 </div>
