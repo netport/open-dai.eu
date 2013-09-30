1005c098825a4e2670cc91be89a5df1819676dab
frebro <frebro@gmail.com>
2013-09-30 10:11:09 +0200

    footer layout

--- app/themes/open-dai/templates/footer.php
+++ app/themes/open-dai/templates/footer.php
@@ -1,16 +1,22 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p class="col-sm-6">
-        <?php if( !is_front_page() ) : ?><a href="<?php echo home_url(); ?>/">Return to start page</a><br><?php endif; ?>
-        The source code for this website is <a href="https://github.com/netport/open-dai.eu">available on GitHub</a>.
-      </p>
-      <p class="col-sm-6">
-        <img src="/<?php echo THEME_PATH; ?>/assets/img/flag_yellow_low.jpg">
-        <img src="/<?php echo THEME_PATH; ?>/assets/img/cip_en-1.gif">
-      </p>
+    <div class="col-xs-12">
+      <?php if( !is_front_page() ) : ?><a class="btn btn-sm btn-info" href="<?php echo home_url(); ?>/">Return to start page</a><br><?php endif; ?>
     </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
+  </div>
+  <div class="row">
+    <p class="col-md-6">
+      The source code for this website is <a href="https://github.com/netport/open-dai.eu">available on GitHub</a>.
+    </p>
+    <p class="col-md-6">
+      <img src="/<?php echo THEME_PATH; ?>/assets/img/flag_yellow_low.jpg">
+      <img src="/<?php echo THEME_PATH; ?>/assets/img/cip_en-1.gif">
+    </p>
   </div>
 </footer>
 
