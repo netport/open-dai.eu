1e8182ef6096a30416410c0a041f028d6fe4fadc
frebro <frebro@gmail.com>
2013-09-30 07:20:04 +0200

    jumbotron responsiveness

--- app/themes/open-dai/lib/scripts.php
+++ app/themes/open-dai/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6453c0843e1f77cae11ab65688a2e3bc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6326bdd263d84acd772db647ae50778');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- app/themes/open-dai/templates/jumbotron.php
+++ app/themes/open-dai/templates/jumbotron.php
@@ -6,7 +6,7 @@
       </div>
       <div class="col-xs-12 col-sm-12 col-md-5 col-md-5">
         <p class="lead">Working toward a European cloud of public administration data and services.</p>
-        <p><abbr title="Software as a Service">SaaS</abbr>&ensp;+&ensp;<abbr title="Platform as a Service">PaaS</abbr>&ensp;+&ensp;<abbr title="Infrastructure as a Service">IaaS</abbr>&emsp;=&emsp;Open-DAI</p>
+        <p><abbr title="Software as a Service">SaaS</abbr>&ensp;+&ensp;<abbr title="Platform as a Service">PaaS</abbr>&ensp;+&ensp;<abbr title="Infrastructure as a Service">IaaS</abbr>&emsp;=&emsp;<abbr title="Opening Data Architechtures and Infrastructures for Public Administrations in the European Union">Open-DAI</abbr></p>
       </div>
     </div>
   </div>
