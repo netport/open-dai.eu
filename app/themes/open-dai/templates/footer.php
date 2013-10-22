3299c792cdd177ef576f26e1c54dbbe3d04e0b0a
frebro <frebro@gmail.com>
2013-10-22 10:53:32 +0200

    added social media links

--- app/themes/open-dai/templates/footer.php
+++ app/themes/open-dai/templates/footer.php
@@ -5,9 +5,12 @@
     </div>
   </div>
   <div class="row">
-    <p class="col-md-6">
-      The source code for this website is <a href="https://github.com/netport/open-dai.eu">available on GitHub</a>.
-    </p>
+    <div class="col-md-6">
+      <p><a href="https://www.facebook.com/cipopendai" target="_blank"><span class="glyphicon glyphicon-arrow-right"></span>&ensp;Follow us on Facebook</a> for more project updates.</p>
+      <p><span class="glyphicon glyphicon-arrow-right"></span>&ensp;Discuss <a href="https://twitter.com/Open_DAI" target="_blank">Open-DAI on Twitter</a>.</p>
+      <p><span class="glyphicon glyphicon-arrow-right"></span>&ensp;Connect with <a href="http://in.linkedin.com/groups/Open-DAI-5097195" target="_blank">partners on LinkedIn</a>.</p>
+      <p>The source code for this website is <a href="https://github.com/netport/open-dai.eu">available on GitHub</a>.</p>
+    </div>
     <p class="col-md-6">
       <img src="/<?php echo THEME_PATH; ?>/assets/img/flag_yellow_low.jpg">
       <img src="/<?php echo THEME_PATH; ?>/assets/img/cip_en-1.gif">
