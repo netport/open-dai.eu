fbda8a856d107ad3a7252b905b27494700ff61c4
frebro <frebro@gmail.com>
2014-06-25 23:54:49 +0200

    moved footer sidebar

--- app/themes/open-dai/templates/footer.php
+++ app/themes/open-dai/templates/footer.php
@@ -1,17 +1,15 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-xs-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-4">
       <p><a href="https://www.facebook.com/cipopendai" target="_blank"><span class="glyphicon glyphicon-arrow-right"></span>&ensp;Follow us on Facebook</a> for more project updates.</p>
       <p><span class="glyphicon glyphicon-arrow-right"></span>&ensp;Discuss <a href="https://twitter.com/Open_DAI" target="_blank">Open-DAI on Twitter</a>.</p>
       <p><span class="glyphicon glyphicon-arrow-right"></span>&ensp;Connect with <a href="http://in.linkedin.com/groups/Open-DAI-5097195" target="_blank">partners on LinkedIn</a>.</p>
       <p>The source code for this website is <a href="https://github.com/netport/open-dai.eu">available on GitHub</a>.</p>
     </div>
-    <p class="col-md-6">
+    <div class="col-md-4">
+      <?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
+    <p class="col-md-4">
       <img src="<?php echo THEME_PATH; ?>/assets/img/flag_yellow_low.jpg">
       <img src="<?php echo THEME_PATH; ?>/assets/img/cip_en-1.gif">
     </p>
