f8b6f69376e340f97d897669c96f8950fd0a9835
frebro <frebro@gmail.com>
2014-10-29 16:37:41 +0100

    set up capistrano deploy to staging server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,32 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'open-dai.eu'
+set :repo_url, 'git@github.com:netport/open-dai.eu.git'
 
 # Branch options
 # Prompts for the branch name (defaults to current branch)
-#ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Sets branch to current one
 #set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Hardcodes branch to always be master
 # This could be overridden in a stage config file
-set :branch, :master
+# set :branch, :master
 
-set :deploy_to, "/srv/www/#{fetch(:application)}"
+set :log_level, :debug
 
-set :log_level, :info
-
-set :linked_files, %w{.env}
-set :linked_dirs, %w{web/app/uploads}
+set :linked_files, %w{.env web/.htaccess}
+set :linked_dirs, %w{web/app/uploads web/app/upgrade}
 
 namespace :deploy do
-  desc 'Restart application'
-  task :restart do
+  desc 'Change ownership of deployed files to server user'
+  task :chown do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :service, :nginx, :reload
+      execute :chown, "-R apache:apache #{release_path}"
+      execute :chown, "-R apache:apache #{current_path}"
     end
   end
 end
 
 # The above restart task is not run by default
 # Uncomment the following line to run it on deploys if needed
-# after 'deploy:publishing', 'deploy:restart'
+after 'deploy:publishing', 'deploy:chown'
