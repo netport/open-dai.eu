e8eaf0f714adec38fda4854af27a73210be7afec
frebro <frebro@gmail.com>
2014-10-30 16:30:46 +0100

    shortcode for glyphicons

--- web/app/themes/open-dai/lib/shortcodes.php
+++ web/app/themes/open-dai/lib/shortcodes.php
@@ -24,3 +24,25 @@ function opendai_nav_shortcode( $atts ) {
   return ob_get_clean();
 }
 add_shortcode( 'nav', 'opendai_nav_shortcode' );
+
+//** [icon name=""]
+function opendai_icon_shortcode( $atts ) {
+
+  // Attribute defaults
+  $atts = shortcode_atts( array(
+    'name'           => null
+  ), $atts, 'icon' );
+
+  // Set the global post variable for setup post data below
+  global $post;
+
+  // Generate output
+  ob_start();
+  if ( !empty($atts['name']) ) : ?>
+    <span class="glyphicon glyphicon-<?php echo $atts['name']; ?>"></span>
+  <?php endif;
+
+  // Return above output
+  return ob_get_clean();
+}
+add_shortcode( 'icon', 'opendai_icon_shortcode' );
