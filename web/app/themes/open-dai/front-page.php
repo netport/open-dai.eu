41a30868f5ccb687e973a7e5ee13b1c0c0d0533e
frebro <frebro@gmail.com>
2014-11-03 11:28:10 +0100

    added tinyletter embed on front-page

--- web/app/themes/open-dai/front-page.php
+++ web/app/themes/open-dai/front-page.php
@@ -4,6 +4,21 @@
 endwhile; ?>
 
 <div class="row">
-  <div class="col-md-6"><?php get_template_part('templates/newslist'); ?></div>
-  <div class="col-md-6"><?php get_template_part('templates/social-embeds'); ?></div>
+  <div class="col-xs-12 col-md-6"><?php get_template_part('templates/newslist'); ?></div>
+  <div class="col-xs-12 col-md-6"><?php get_template_part('templates/social-embeds'); ?></div>
+</div>
+
+<div class="row">
+  <div style="margin-top:40px;" class="col-xs-12 col-md-8 col-md-offset-2">
+    <h3>Open-DAI newsletter</h3>
+    <p>Sign up with your email address to receive updates about Open-DAI directly to your inbox</p>
+    <form action="https://tinyletter.com/open-dai" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/open-dai', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+      <div class="form-group">
+        <label for="tlemail">Your email address</label>
+        <input type="email" class="form-control" name="email" id="tlemail" />
+      </div>
+      <input type="hidden" value="1" name="embed"/>
+      <input type="submit" class="btn btn-default" value="Subscribe" />
+    </form>
+  </div>
 </div>
